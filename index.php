c0abbfe9bcf5d80f91cbd4e87eef22eee87ebab3
cobolbaby@qq.com <cobolbaby@qq.com>
2017-05-20 00:09:22 +0800

    define the entry page of tp

--- index.php
+++ index.php
@@ -1,50 +1,11 @@
 <?php
 
-// +----------------------------------------------------------------------
-
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-
-// +----------------------------------------------------------------------
-
-// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
-
-// +----------------------------------------------------------------------
-
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-
-// +----------------------------------------------------------------------
-
-// | Author: liu21st <liu21st@gmail.com>
-
-// +----------------------------------------------------------------------
-
-
-
-// 应用入口文件
-
-
-
-// 检测PHP环境
-
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
-
-
-
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-
-define('APP_DEBUG',false);
-
+define('APP_DEBUG', false);
+// 重新定义Runtime路径
+define('RUNTIME_PATH', '/tmp/boaihui/Runtime/User/');
 
 // 定义应用目录
-
-define('APP_PATH','./User/');
-
-
-
+define('APP_PATH', './User/');
 // 引入ThinkPHP入口文件
-
 require './ThinkPHP/ThinkPHP.php';
-
-
-
-// 亲^_^ 后面不需要任何代码了 就是如此简单
